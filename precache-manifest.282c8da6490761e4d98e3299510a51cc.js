37b389cc220b4954476f2bb24659641df1cd0da4
niyaaz_alpha <niyaaz79red@gmail.com>
2020-09-24 00:09:18 +0200

    Updates

---
+++ precache-manifest.282c8da6490761e4d98e3299510a51cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aebe89ec9283c875567b03c7a270fca",
+    "url": "/credit_card_validator/index.html"
+  },
+  {
+    "revision": "fc310d4750fbad2bffad",
+    "url": "/credit_card_validator/static/css/main.8e228bd2.chunk.css"
+  },
+  {
+    "revision": "e137ccc334dbb7586b7e",
+    "url": "/credit_card_validator/static/js/2.b84c487e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/credit_card_validator/static/js/2.b84c487e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc310d4750fbad2bffad",
+    "url": "/credit_card_validator/static/js/main.59ed0742.chunk.js"
+  },
+  {
+    "revision": "3a02946eb8fbc8366107",
+    "url": "/credit_card_validator/static/js/runtime-main.508a144b.js"
+  }
+]);
